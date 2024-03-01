d2bca1f7431aff68f2343700c9e7bcc5c91d44e3
Davy Verstappen <davy.verstappen@iodigital.com>
2024-03-01 11:07:08 +0100

    Added a script to configure/update the mirroring to GitHub

---
+++ bin/ax-github-mirror.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import os
+import pprint
+
+import gitlab
+import typer
+
+from across.build import GITLAB_PAT
+from across.config import AcrossConfig
+
+GITHUB_PAT = "GITHUB_PAT"
+
+app = typer.Typer(
+    help="List repository or module names, for use in shell scripts such as ax-exec.sh"
+)
+
+
+@app.command(help="Configure mirroring to the public GitHub repositories")
+def configure(gh_user_name: str):
+    pat = os.environ.get(GITLAB_PAT)
+    if not pat:
+        raise Exception(
+            f"Environment variable {GITLAB_PAT} (personal access token) is not set."
+        )
+    gh_pat = os.environ.get(GITHUB_PAT)
+    if not gh_pat:
+        raise Exception(
+            f"Environment variable {GITHUB_PAT} (personal access token) is not set."
+        )
+    _, config = AcrossConfig.load()
+    gl = gitlab.Gitlab(
+        url="https://gitlab.isaac.nl",  # TODO remove hardcoded URL
+        private_token=pat,
+        user_agent="antwerpen/across.py",
+    )
+    gl.auth()
+    for repo_config in config.repositories:
+        print(repo_config.name)
+        repo_name = repo_config.name
+        # if repo_name != "across-framework":
+        #     return
+        _update_repo(gl, repo_name, gh_pat, gh_user_name)
+    _update_repo(gl, "across-development", gh_pat, gh_user_name)
+
+
+def _update_repo(gl, repo_name, gh_pat, gh_user_name):
+    project = gl.projects.get(f"antwerpen/across/{repo_name}")
+    mirror_mgr = project.remote_mirrors
+    gh_url = f"https://{gh_user_name}:{gh_pat}@github.com/foreach-across/{repo_name}.git"
+    result = None
+    for m in mirror_mgr.list():
+        if "/foreach-across/" in m.url:
+            result = mirror_mgr.update(m.id, url=gh_url)
+        else:
+            print(m.url)
+    if not result:
+        result = mirror_mgr.create({
+            "url": gh_url,
+            "enabled": True,
+            "auth_method": "password",
+        })
+    pprint.pp(result)
+
+
+if __name__ == "__main__":
+    app()
--- docs/regular-maintenance.md
+++ docs/regular-maintenance.md
@@ -4,6 +4,12 @@ weight: 90
 toc: true
 ---
 
+This page describes regular maintenance that needs to be performed,
+such as rotating tokens and keys.
+
+<!--more-->
+
+
 ## GitHub sync token rotation
 
 The internal GitLab repositories are automatically synched to
@@ -15,6 +21,11 @@ For documentation to configure the sync, see:
 - https://docs.gitlab.com/ee/user/project/repository/mirror/
 - https://docs.gitlab.com/ee/user/project/repository/mirror/push.html
 - https://docs.gitlab.com/ee/user/project/repository/mirror/push.html#set-up-a-push-mirror-from-gitlab-to-github
+
+The current token will expire on 2025-03-01.
+
+The `ax-github-mirror.py` script can be used to configure/update the
+mirroring.
 
 
 ## GPG key rotation
